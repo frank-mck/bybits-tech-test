44588b5d90b86887549f403f6c8895581fad5426
Frank Mckenna <fmckenna56330@gmail.com>
2021-10-08 19:05:18 +0100

    chore: redirects user if there is no token

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import './App.css';
 import React, { useState } from 'react';
 import SignIn from './components/pages/SignIn/SignIn';
 import UserPolicy from './components/pages/Policy/UserPolicy';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, useHistory } from 'react-router-dom';
 
 const App = () => {
   const [token, setToken] = useState('');
+
+  const history = useHistory();
+
+  if (!token) {
+    history.push('/')
+  }
 
   return (
     <Router>
